e17af4cf26e28e9c9ebf67efe624511ff3e51ec4
Leumor <116955025+leumor@users.noreply.github.com>
2025-07-28 20:00:42 +0100

    Update the icon for Crash dialog

--- CrashDialog.Designer.cs
+++ CrashDialog.Designer.cs
@@ -29,82 +29,82 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CrashDialog));
-            this.IconBox = new System.Windows.Forms.PictureBox();
-            this.crashMessageLabel = new System.Windows.Forms.Label();
-            this.ViewLogButton = new System.Windows.Forms.Button();
-            this.SupportChatButton = new System.Windows.Forms.Button();
-            this.MailingListButton = new System.Windows.Forms.Button();
-            this.RestartButton = new System.Windows.Forms.Button();
-            this.CloseButton = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.IconBox)).BeginInit();
-            this.SuspendLayout();
+            IconBox = new System.Windows.Forms.PictureBox();
+            crashMessageLabel = new System.Windows.Forms.Label();
+            ViewLogButton = new System.Windows.Forms.Button();
+            SupportChatButton = new System.Windows.Forms.Button();
+            MailingListButton = new System.Windows.Forms.Button();
+            RestartButton = new System.Windows.Forms.Button();
+            CloseButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)IconBox).BeginInit();
+            SuspendLayout();
             // 
             // IconBox
             // 
-            resources.ApplyResources(this.IconBox, "IconBox");
-            this.IconBox.Name = "IconBox";
-            this.IconBox.TabStop = false;
+            resources.ApplyResources(IconBox, "IconBox");
+            IconBox.Name = "IconBox";
+            IconBox.TabStop = false;
             // 
             // crashMessageLabel
             // 
-            resources.ApplyResources(this.crashMessageLabel, "crashMessageLabel");
-            this.crashMessageLabel.Name = "crashMessageLabel";
+            resources.ApplyResources(crashMessageLabel, "crashMessageLabel");
+            crashMessageLabel.Name = "crashMessageLabel";
             // 
             // ViewLogButton
             // 
-            resources.ApplyResources(this.ViewLogButton, "ViewLogButton");
-            this.ViewLogButton.Name = "ViewLogButton";
-            this.ViewLogButton.UseVisualStyleBackColor = true;
-            this.ViewLogButton.Click += new System.EventHandler(this.ViewLogButton_Click);
+            resources.ApplyResources(ViewLogButton, "ViewLogButton");
+            ViewLogButton.Name = "ViewLogButton";
+            ViewLogButton.UseVisualStyleBackColor = true;
+            ViewLogButton.Click += ViewLogButton_Click;
             // 
             // SupportChatButton
             // 
-            resources.ApplyResources(this.SupportChatButton, "SupportChatButton");
-            this.SupportChatButton.Name = "SupportChatButton";
-            this.SupportChatButton.UseVisualStyleBackColor = true;
-            this.SupportChatButton.Click += new System.EventHandler(this.SupportChatButton_Click);
+            resources.ApplyResources(SupportChatButton, "SupportChatButton");
+            SupportChatButton.Name = "SupportChatButton";
+            SupportChatButton.UseVisualStyleBackColor = true;
+            SupportChatButton.Click += SupportChatButton_Click;
             // 
             // MailingListButton
             // 
-            resources.ApplyResources(this.MailingListButton, "MailingListButton");
-            this.MailingListButton.Name = "MailingListButton";
-            this.MailingListButton.UseVisualStyleBackColor = true;
-            this.MailingListButton.Click += new System.EventHandler(this.MailingListButton_Click);
+            resources.ApplyResources(MailingListButton, "MailingListButton");
+            MailingListButton.Name = "MailingListButton";
+            MailingListButton.UseVisualStyleBackColor = true;
+            MailingListButton.Click += MailingListButton_Click;
             // 
             // RestartButton
             // 
-            resources.ApplyResources(this.RestartButton, "RestartButton");
-            this.RestartButton.Name = "RestartButton";
-            this.RestartButton.UseVisualStyleBackColor = true;
-            this.RestartButton.Click += new System.EventHandler(this.RestartButton_Click);
+            resources.ApplyResources(RestartButton, "RestartButton");
+            RestartButton.Name = "RestartButton";
+            RestartButton.UseVisualStyleBackColor = true;
+            RestartButton.Click += RestartButton_Click;
             // 
             // CloseButton
             // 
-            this.CloseButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            resources.ApplyResources(this.CloseButton, "CloseButton");
-            this.CloseButton.Name = "CloseButton";
-            this.CloseButton.UseVisualStyleBackColor = true;
-            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            CloseButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(CloseButton, "CloseButton");
+            CloseButton.Name = "CloseButton";
+            CloseButton.UseVisualStyleBackColor = true;
+            CloseButton.Click += CloseButton_Click;
             // 
             // CrashDialog
             // 
             resources.ApplyResources(this, "$this");
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.CancelButton = this.CloseButton;
-            this.Controls.Add(this.CloseButton);
-            this.Controls.Add(this.RestartButton);
-            this.Controls.Add(this.MailingListButton);
-            this.Controls.Add(this.SupportChatButton);
-            this.Controls.Add(this.ViewLogButton);
-            this.Controls.Add(this.crashMessageLabel);
-            this.Controls.Add(this.IconBox);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "CrashDialog";
-            this.Load += new System.EventHandler(this.CrashDialog_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.IconBox)).EndInit();
-            this.ResumeLayout(false);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            CancelButton = CloseButton;
+            Controls.Add(CloseButton);
+            Controls.Add(RestartButton);
+            Controls.Add(MailingListButton);
+            Controls.Add(SupportChatButton);
+            Controls.Add(ViewLogButton);
+            Controls.Add(crashMessageLabel);
+            Controls.Add(IconBox);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "CrashDialog";
+            Load += CrashDialog_Load;
+            ((System.ComponentModel.ISupportInitialize)IconBox).EndInit();
+            ResumeLayout(false);
 
         }
 
